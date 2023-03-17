24f9e72a96209e53053565a4de5ee925ac17c218
Kozachek Yury <show_@mail.ru>
2023-03-17 21:40:57 +0300

    add schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
